168f793d48b8e6f7fa8be4d1cd102115bd1e393a
jafow <jared.a.fowler@gmail.com>
2017-12-14 14:17:24 -0800

    2.2

--- 2.py
+++ 2.py
@@ -1,4 +1,5 @@
 import unittest
+from itertools import combinations
 
 """
 For each row, determine the difference between the largest value and the smallest value; the checksum is the sum of all of these differences.
@@ -24,18 +25,40 @@ def checksums(rows):
     res = [list(map(int, row)) for row in rows]
     return sum(max(row) - min(row) for row in res)
 
+
+def checkdivisor(rows):
+    res = [list(map(int, row)) for row in rows]
+    return sum(
+            max(t) / min(t)
+            for row in res
+            for t in combinations(row, 2)
+            if max(t) % min(t) == 0
+            )
+
+
 def setup():
-    f = open('input2.txt', 'r')
+    f = open('./input/input2.txt', 'r')
     rows = [row.rstrip().split('\t') for row in f.readlines()]
-    print(checksums(rows))
+    # print(checksums(rows))
+    print(checkdivisor(rows))
 
 
 setup()
+
 # Tests
+
+
 class TestCheckSums(unittest.TestCase):
     def test_sums_row_min_max_differences(self):
         self.assertEqual(checksums([[1, 2, 3], [5, 2, 4]]), 5)
-        self.assertEqual(checksums([[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]), 18)
+        self.assertEqual(
+                checksums([[5, 1, 9, 5], [7, 5, 3], [2, 4, 6, 8]]),
+                18)
+
+    def test_checkdivisor(self):
+        rows = [[5, 9, 2, 8], [9, 4, 7,  3],  [3, 8,  6, 5]]
+        self.assertEqual(checkdivisor(rows), 9)
+
 
 if __name__ == '__main__':
     unittest.main()
