b0879c7f78645252173cf77400a8ff76022bab09
jafow <jared.a.fowler@gmail.com>
2017-12-25 11:01:04 -0800

    8.1
    8.2

--- 8.py
+++ 8.py
@@ -33,3 +33,85 @@ are named, and leaves that to you to determine.
 What is the largest value in any register after completing the instructions in
 your puzzle input?
 """
+
+
+def gt(a, b): return a > b
+
+
+def lt(a, b): return a < b
+
+
+def eq(a, b): return a == b
+
+
+def neq(a, b): return a != b
+
+
+def gte(a, b): return a >= b
+
+
+def lte(a, b): return a <= b
+
+
+def inc(a, b): return a + b
+
+
+def dec(a, b): return a - b
+
+
+# table of comparison operators & methods to perform those comparisons
+oper_table = {
+        '>': gt,
+        '<': lt,
+        '==': eq,
+        '!=': neq,
+        '>=': gte,
+        '<=': lte,
+        'inc': inc,
+        'dec': dec
+        }
+
+
+def parse_cond(pieces, regs):
+
+    """ parse a condition from pieces; return result of applying the condition
+    to pieces """
+
+    reg, op, val = pieces
+    if reg not in regs:
+        regs[reg] = 0
+
+    return oper_table[op](regs[reg], int(val))
+
+
+def modify(regs, targ, op, A):
+
+    """ modify a target targ in regs by amount A using operator op """
+
+    if targ not in regs:
+        regs[targ] = 0
+
+    action = oper_table[op]
+    regs[targ] = action(regs[targ], A)
+    return regs[targ]
+
+
+def main(lines):
+    regs = {}
+    highest = -10000
+
+    for line in lines:
+        targ_reg, op, val, *cond = line.split(' ')
+
+        if targ_reg not in regs:
+            regs[targ_reg] = 0
+
+        if parse_cond(cond[1:], regs):
+            highest = max(highest, modify(regs, targ_reg, op, int(val)))
+
+    m = max(regs.values())
+    print(highest)
+    return m
+
+
+main(open('./input/8.txt'))
